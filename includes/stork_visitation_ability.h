4b9b8fc36eab5c5085baa3a61feb1abaea896137
Anthony Chen <31290848+slightlyskepticalpotat@users.noreply.github.com>
2025-07-29 16:20:18 -0400

    unnecessary comment

--- includes/stork_visitation_ability.h
+++ includes/stork_visitation_ability.h
@@ -7,7 +7,6 @@ class StorkVisitationAbility : public Ability {
   public:
     explicit StorkVisitationAbility(Player* owner);
 
-    // args[0] is the ID of the link to boost. must be owned by the player
     bool use(Game& game, std::istream& in) override;
 
     std::string getName() const override;
